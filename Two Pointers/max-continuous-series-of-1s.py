c22aa762af1265b86537429ce3f82cb23ed055f6
sameer ranjan <40149707+wilfredarin@users.noreply.github.com>
2020-08-17 17:13:24 +0530

    Update max-continuous-series-of-1s.py

--- Two Pointers/max-continuous-series-of-1s.py
+++ Two Pointers/max-continuous-series-of-1s.py
@@ -1,21 +1,28 @@
 class Solution:
     # @param A : list of integers
     # @param B : integer
-    # @return an integer
-    def removeElement(self, A, B):
-        """
-        def removeElement(self, A, B):
-        A[:] = [ii for ii in A if ii != B]
-        return len(A)
-        """
-        cur = 0
-        t =0 
-        for i in range(len(A)):
-            if A[i]!=B:
-                A[cur]=A[i]
-                cur+=1
-            else:
-                t+=1
-        A =A[:len(A)-t]
-        return len(A)
+    # @return a list of integers
+    def maxone(self, A, B):
+        eow = -1
+        sow = 0
+        max_sow,max_eow = 0,0
         
+        for i in A:
+            eow+=1
+            if i==0:
+                if B==0:
+                    if max_eow-max_sow<eow-sow:
+                        max_eow = eow
+                        max_sow = sow
+                    while A[sow]!=0:
+                        sow+=1
+                    sow+=1 
+                else:
+                    B-=1 
+        if max_eow-max_sow<eow-sow+1:
+            return range(sow,eow+1)
+        return range(max_sow,max_eow)
+                    
+                    
+                    
+                    
